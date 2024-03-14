4f8d86dad6505c187f097d21d23517c775656aef
timka <timkaa555@gmail.com>
2024-03-14 19:44:44 +0500

    refactor

--- src/main/java/org/beauty/Main.java
+++ src/main/java/org/beauty/Main.java
@@ -3,8 +3,12 @@ package org.beauty;
 
 import org.beauty.db.DatabaseInitializer;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
+
         DatabaseInitializer.initializeDatabase();
     }
 
